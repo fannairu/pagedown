bb46607ce025a4486d96e3881da6ee6ea380cf7e
fannairu <fannairu@gmail.com>
2015-08-31 11:26:49 +0800

    add editor

--- node-pagedown.js
+++ node-pagedown.js
@@ -1,2 +1,3 @@
 exports.Converter = require("./Markdown.Converter").Converter;
 exports.getSanitizingConverter = require("./Markdown.Sanitizer").getSanitizingConverter;
+exports.Editor = require("./Markdown.Editor").Editor;
